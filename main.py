c29e95dafb79215f096dd06668ff1010ff23390c
dokiugit <120786808+dokiugit@users.noreply.github.com>
2026-05-12 19:44:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from utils.extract import scrape_all
+from utils.transform import transform_data
+from utils.load import load_to_csv
+
+
+def main():
+    print("Extracting...")
+    raw_data = scrape_all()
+
+    print("Transforming...")
+    clean_data = transform_data(raw_data)
+
+    print("Loading...")
+    load_to_csv(clean_data)
+
+
+if __name__ == "__main__":
+    main()
 
